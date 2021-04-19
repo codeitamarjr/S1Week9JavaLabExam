c6a368c40ac4192eaeea0276a17409cd4d6d5a35
Itamar Junior <itjunior@gmail.com>
2021-04-19 23:58:49 +0100

    Fixed boolean

--- src/LotteryExercise.java
+++ src/LotteryExercise.java
@@ -5,7 +5,10 @@ public class LotteryExercise {
 
         //Student ID 23952 Itamar Atanasio da Silva Junior
 
+        //starting three random numbers between 1 and 24
         int winingNumber1= 1+(int)(Math.random()*24), winingNumber2= 1+(int)(Math.random()*24) , winingNumber3= 1+(int)(Math.random()*24);
+
+        //checking if all the numbers are different
         while(winingNumber1 == winingNumber2){	winingNumber2 = 1+(int)(Math.random()*24);} // to check the 1st and 2nd numbers be dif
         while (winingNumber3 == winingNumber1 || winingNumber3 == winingNumber2){	winingNumber3 = 1+(int)(Math.random()*24);} //to check if the 3rd number is dif of 1 and 2.
 
@@ -16,6 +19,7 @@ public class LotteryExercise {
         System.out.println("Enter the first number for your lottery ticket(Between 1 and 24):");
         lotteryNumber1 = keyboard.nextInt();
 
+        //Check if the user input a valid number
         while(lotteryNumber1 < 1 || lotteryNumber1 > 24){
             System.out.println("Try again using a number between 1 and 24");
             lotteryNumber1 = keyboard.nextInt();
@@ -24,6 +28,7 @@ public class LotteryExercise {
         System.out.println("Enter the second number for your lottery ticket(Between 1 and 24):");
         lotteryNumber2 = keyboard.nextInt();
 
+        //check if the second number is valid and different of the others
         while(lotteryNumber2 == lotteryNumber1 || (lotteryNumber2 < 1 || lotteryNumber2 > 24)){
             System.out.println("Enter the second number again and chose a different number between 1 and 24:");
             lotteryNumber2 = keyboard.nextInt();
@@ -32,6 +37,7 @@ public class LotteryExercise {
         System.out.println("Enter the third number for your lottery ticket(Between 1 and 24):");
         lotteryNumber3 = keyboard.nextInt();
 
+        //check if the third number is valid and different of the others
         while (lotteryNumber3 == lotteryNumber2 || lotteryNumber3 == lotteryNumber1 || (lotteryNumber3 < 1 || lotteryNumber3 > 24)){
             System.out.println("Enter the third number again and chose a different number(Between 1 and 24:");
             lotteryNumber3 = keyboard.nextInt();
@@ -42,29 +48,28 @@ public class LotteryExercise {
         System.out.println("Your lottery ticket number is:"+lotteryNumber1+","+lotteryNumber2+","+lotteryNumber3);
         System.out.println("The result of this lottery is:"+winingNumber1+","+winingNumber2+","+winingNumber3);
 
-        //The statement below check if the first number match with one of the winingNumber AND
-        //if the second number match with one of the winingNumber AND
-        //if the third number match with on of the winingNumber
-        //so the name won the first prize
-
+        //The statement bellow create one boolean for each number to check if the users have a match
+        //check if the first number match with one of the random numbers to create one match and so on...
         boolean firstMatch = (lotteryNumber1==winingNumber1 || lotteryNumber1==winingNumber2 || lotteryNumber1==winingNumber3);
         boolean secondMatch = (lotteryNumber2==winingNumber1 || lotteryNumber2==winingNumber2 || lotteryNumber2==winingNumber3);
         boolean thirdMatch = (lotteryNumber3==winingNumber1 || lotteryNumber3==winingNumber2 || lotteryNumber3==winingNumber3);
 
+        //If all the numbers match the user won the first prize
         if (firstMatch && secondMatch && thirdMatch)
             System.out.println("Congratulations! You have won the first prize.");
             else {
 
+                //if not and the user have two match he won the second prize
             if ( (firstMatch && secondMatch) || (firstMatch && thirdMatch) || (secondMatch && thirdMatch))
                 System.out.println("Congratulations! You have won the second prize.");
 
             else {
-                //The statement below if one of those three match
+                //The statement below if just one of those three match
                 if (firstMatch || secondMatch || thirdMatch)
                     System.out.println("Congratulations! You have won the third prize.");
             }
             }
-
+            //If none of them match Java will print the statement bellow
             System.out.println("Best luck next time!");
     }
 }
